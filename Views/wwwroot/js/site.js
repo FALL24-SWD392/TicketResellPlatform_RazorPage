3b707969ca33d9c3f74142b44c715297735e3094
neru <ngocngocthuc@gmail.com>
2024-11-11 00:21:30 +0700

    build(config): config for signalr

--- Views/wwwroot/js/site.js
+++ Views/wwwroot/js/site.js
@@ -1,4 +1,7 @@
-﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿"use strict";
 
-// Write your JavaScript code.
+$(async () => {
+    const connection = new signalR.HubConnectionBuilder().withUrl("/signalrhub").build();
+    await connection.start();
+
+})
