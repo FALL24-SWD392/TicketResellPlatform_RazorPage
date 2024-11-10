9a8ba1ceafddc5ee23f8dbe5f521b80866ae0ae7
neru <ngocngocthuc@gmail.com>
2024-11-10 23:43:47 +0700

    first commit

---
+++ Daos/IDao.cs
@@ -0,0 +1,11 @@
+﻿namespace Daos
+{
+    public interface IDao<T>
+    {
+        Task<T> GetAsync(string id);
+        Task<IList<T>> GetAllAsync();
+        Task<T> AddAsync(T entity);
+        Task<T> UpdateAsync(T entity);
+        Task<T> DeleteAsync(string id);
+    }
+}
---
+++ Repositories/IRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace Repositories
+{
+    public interface IRepository<T>
+    {
+        Task<T> GetAsync(string id);
+        Task<IList<T>> GetAllAsync();
+        Task<T> AddAsync(T entity);
+        Task<T> UpdateAsync(T entity);
+        Task<T> DeleteAsync(string id);
+    }
+}
