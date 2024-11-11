c2c47aca25f060ae45c124a5e98d666cecbc5b21
neru <ngocngocthuc@gmail.com>
2024-11-12 00:04:33 +0700

    feat(repository): subscription repository

---
+++ Repositories/SubscriptionRepository/SubscriptionRepository.cs
@@ -0,0 +1,38 @@
+﻿using Business;
+using Daos.SubsciptionDao;
+
+namespace Repositories.SubscriptionRepository
+{
+    public class SubscriptionRepository : ISubscriptionRepository
+    {
+        private static SubscriptionRepository instance;
+        private readonly ISupscriptionDao dao;
+        private SubscriptionRepository()
+        {
+            dao = SupsciptionDao.Instance;
+        }
+        private static SubscriptionRepository Instance
+        {
+            get
+            {
+                instance ??= new();
+                return instance;
+            }
+        }
+
+        public async Task<Subcription?> AddAsync(Subcription entity)
+            => await dao.AddAsync(entity);
+
+        public async Task<Subcription?> DeleteAsync(int id)
+            => await dao.DeleteAsync(id);
+
+        public async Task<IList<Subcription>> GetAllAsync()
+            => await dao.GetAllAsync();
+
+        public async Task<Subcription?> GetAsync(int id)
+            => await dao.GetAsync(id);
+
+        public async Task<Subcription?> UpdateAsync(Subcription entity)
+            => await dao.UpdateAsync(entity);
+    }
+}
