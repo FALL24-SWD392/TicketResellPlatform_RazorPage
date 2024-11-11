1d187991343fb4d05ff57f1282dc7c8ce95c86b7
neru <ngocngocthuc@gmail.com>
2024-11-11 23:08:17 +0700

    feat(service): user service

---
+++ Services/UserService/UserService.cs
@@ -0,0 +1,38 @@
+﻿using Business;
+using Repositories.UserRepository;
+
+namespace Services.UserService
+{
+    public class UserService : IUserService
+    {
+        private static UserService? instance;
+        private readonly IUserRepository userRepo;
+        private UserService()
+        {
+            userRepo = UserRepository.Instance;
+        }
+        public static UserService Instance
+        {
+            get
+            {
+                instance ??= new();
+                return instance;
+            }
+        }
+
+        public async Task<User?> AddAsync(User entity)
+            => await userRepo.AddAsync(entity);
+
+        public async Task<User?> DeleteAsync(int id)
+            => await userRepo.DeleteAsync(id);
+
+        public async Task<IList<User>> GetAllAsync()
+            => await userRepo.GetAllAsync();
+
+        public async Task<User?> GetAsync(int id)
+            => await userRepo.GetAsync(id);
+
+        public async Task<User?> UpdateAsync(User entity)
+            => await userRepo.UpdateAsync(entity);
+    }
+}
