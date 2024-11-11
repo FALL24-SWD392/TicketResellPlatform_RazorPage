8e81996024078564c923546c65d7c7318c27b3d3
neru <ngocngocthuc@gmail.com>
2024-11-11 23:55:27 +0700

    feat(repository): ticket repository

---
+++ Repositories/TicketRepository/TicketRepository.cs
@@ -0,0 +1,38 @@
+﻿using Business;
+using Daos.TicketDao;
+
+namespace Repositories.TicketRepository
+{
+    public class TicketRepository : ITicketRepository
+    {
+        private static TicketRepository? instance;
+        private readonly ITicketDao dao;
+        private TicketRepository()
+        {
+            dao = TicketDao.Instance;
+        }
+        private static TicketRepository Instance
+        {
+            get
+            {
+                instance ??= new();
+                return instance;
+            }
+        }
+
+        public async Task<Ticket?> AddAsync(Ticket entity)
+            => await dao.AddAsync(entity);
+
+        public async Task<Ticket?> DeleteAsync(int id)
+            => await dao.DeleteAsync(id);
+
+        public async Task<IList<Ticket>> GetAllAsync()
+            => await dao.GetAllAsync();
+
+        public async Task<Ticket?> GetAsync(int id)
+            => await dao.GetAsync(id);
+
+        public async Task<Ticket?> UpdateAsync(Ticket entity)
+            => await dao.UpdateAsync(entity);
+    }
+}
