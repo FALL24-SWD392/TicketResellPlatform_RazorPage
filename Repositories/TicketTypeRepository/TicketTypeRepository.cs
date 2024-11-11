422a5dfe0ca2545d4a7d739660c02e44cc8b0ba8
neru <ngocngocthuc@gmail.com>
2024-11-11 23:58:20 +0700

    feat(repository): ticket type repository

---
+++ Repositories/TicketTypeRepository/TicketTypeRepository.cs
@@ -0,0 +1,38 @@
+﻿using Business;
+using Daos.TicketTypeDao;
+
+namespace Repositories.TicketTypeRepository
+{
+    public class TicketTypeRepository : ITicketTypeRepository
+    {
+        private static TicketTypeRepository? instance;
+        private readonly ITicketTypeDao dao;
+        private TicketTypeRepository()
+        {
+            dao = TicketTypeDao.Instance;
+        }
+        private static TicketTypeRepository Instance
+        {
+            get
+            {
+                instance ??= new();
+                return instance;
+            }
+        }
+
+        public async Task<TicketType?> AddAsync(TicketType entity)
+            => await dao.AddAsync(entity);
+
+        public async Task<TicketType?> DeleteAsync(int id)
+            => await dao.DeleteAsync(id);
+
+        public async Task<IList<TicketType>> GetAllAsync()
+            => await dao.GetAllAsync();
+
+        public async Task<TicketType?> GetAsync(int id)
+            => await dao.GetAsync(id);
+
+        public async Task<TicketType?> UpdateAsync(TicketType entity)
+            => await dao.UpdateAsync(entity);
+    }
+}
