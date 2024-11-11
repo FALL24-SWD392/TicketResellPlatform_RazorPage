a900fe7d682a54c2975588a5770e413b51f3b150
neru <ngocngocthuc@gmail.com>
2024-11-11 17:04:21 +0700

    refactor(dbcontext): moving context to namespace daos

--- Daos/TicketResellContext.cs
+++ Daos/TicketResellContext.cs
@@ -1,8 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using Microsoft.EntityFrameworkCore;
+using Business;
 
-namespace Business;
+namespace Daos;
 
 public partial class TicketResellContext : DbContext
 {
