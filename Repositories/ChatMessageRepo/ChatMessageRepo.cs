178bb8554bf58f7f4ecabfc3579780b910b081e9
Krit Dat <datnhse170330@fpt.edu.vn>
2024-11-12 10:43:57 +0700

    feat(Chatbox, ChatMessage, Feedback): add CRUD dao, repo, service; Refactor id IDao, IRepository, IService; Refactor Connection String

---
+++ Repositories/ChatMessageRepo/ChatMessageRepo.cs
@@ -0,0 +1,26 @@
+﻿using Business;
+using Daos.ChatMessageDao;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repositories.ChatMessageRepo
+{
+    public class ChatMessageRepo : IChatMessageRepo
+    {
+        private static ChatMessageRepo? instance;
+        public static ChatMessageRepo Instance { get { instance ??= new(); return instance; } }
+
+        public async Task<ChatMessage?> AddAsync(ChatMessage entity) => await ChatMessageDao.Instance.AddAsync(entity);
+
+        public async Task<ChatMessage?> DeleteAsync(int id) => await ChatMessageDao.Instance.DeleteAsync(id);
+
+        public async Task<IList<ChatMessage>> GetAllAsync() => await ChatMessageDao.Instance.GetAllAsync();
+
+        public async Task<ChatMessage?> GetAsync(int id) => await ChatMessageDao.Instance.GetAsync(id);
+
+        public async Task<ChatMessage?> UpdateAsync(ChatMessage entity) => await ChatMessageDao.Instance.UpdateAsync(entity);
+    }
+}
--- Repositories/IRepository.cs
+++ Repositories/IRepository.cs
@@ -2,10 +2,10 @@
 {
     public interface IRepository<T>
     {
-        Task<T?> GetAsync(string id);
+        Task<T?> GetAsync(int id);
         Task<IList<T>> GetAllAsync();
         Task<T?> AddAsync(T entity);
         Task<T?> UpdateAsync(T entity);
-        Task<T?> DeleteAsync(string id);
+        Task<T?> DeleteAsync(int id);
     }
 }
