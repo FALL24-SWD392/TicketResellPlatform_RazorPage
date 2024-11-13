b0afab2f6e77bbf262db1bc5178016321cbb697b
Krit Dat <datnhse170330@fpt.edu.vn>
2024-11-13 10:58:28 +0700

    feat(FeedbackPage): Create Read Page; remove IService<ChatMessage>

--- Services/ChatService/ChatService.cs
+++ Services/ChatService/ChatService.cs
@@ -30,8 +30,6 @@ namespace Services.ChatService
 
         public async Task<Chatbox?> AddAsync(Chatbox entity) => await chatboxRepository.AddAsync(entity);
 
-        public async Task<ChatMessage?> AddAsync(ChatMessage entity) => await chatMessageRepository.AddAsync(entity);
-
         public async Task<Chatbox?> DeleteAsync(int id) => await chatboxRepository.DeleteAsync(id);
 
         public async Task<IList<Chatbox>> GetAllAsync() => await chatboxRepository.GetAllAsync();
@@ -39,13 +37,5 @@ namespace Services.ChatService
         public async Task<Chatbox?> GetAsync(int id) => await chatboxRepository.GetAsync(id);
 
         public async Task<Chatbox?> UpdateAsync(Chatbox entity) => await chatboxRepository.UpdateAsync(entity);
-
-        public async Task<ChatMessage?> UpdateAsync(ChatMessage entity) => await chatMessageRepository.UpdateAsync(entity);
-
-        async Task<ChatMessage?> IService<ChatMessage>.DeleteAsync(int id) => await chatMessageRepository.DeleteAsync(id);
-
-        async Task<IList<ChatMessage>> IService<ChatMessage>.GetAllAsync() => await chatMessageRepository.GetAllAsync();
-
-        async Task<ChatMessage?> IService<ChatMessage>.GetAsync(int id) => await chatMessageRepository.GetAsync(id);
     }
 }
