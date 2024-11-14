e139e51c3349a4ef169c0349b1ae771a0ead37da
neru <ngocngocthuc@gmail.com>
2024-11-14 17:26:25 +0700

    style: chat rooom

---
+++ Views/Pages/Chats/Chatroom.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Views.Pages.Chats
+{
+    public class ChatroomModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
