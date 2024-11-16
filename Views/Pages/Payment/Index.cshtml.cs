a9dda38dc1ace4175ffbdae8a32a77353c6336ea
neru <ngocngocthuc@gmail.com>
2024-11-16 15:38:14 +0700

    fix: payment

--- Views/Pages/Payment/Index.cshtml.cs
+++ Views/Pages/Payment/Index.cshtml.cs
@@ -2,23 +2,18 @@ using Business;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Services.PayOSService;
+using Services.SubscriptionService;
 
 namespace Views.Pages.Payment
 {
-    public class IndexModel(IPayOSService payOSService) : PageModel
+    public class IndexModel(IPayOSService payOSService, ISubscriptionService subscriptionService) : PageModel
     {
         [BindProperty] public Membership Membership { get; set; } = null!;
         [BindProperty] public Subcription Subcription { get; set; } = null!;
-        public async Task<IActionResult> OnGetAsync(int id)
+        public async Task<IActionResult> OnGetAsync(int subscriptionId)
         {
-            Subcription = new()
-            {
-                Description = "Monthly Subscription",
-                Price = 100000,
-                SaleLimit = 10,
-                Name = "Monthly Subscription",
-            };
-            Membership = await payOSService.GetPaymentLink(id);
+            Subcription = await subscriptionService.GetAsync(subscriptionId);
+            Membership = await payOSService.GetPaymentLink(subscriptionId);
             return Page();
         }
     }
